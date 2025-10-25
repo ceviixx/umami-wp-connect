15d48c90d72066e125e436e10ff44f5e94b84fff
clumsy <31499125+ceviixx@users.noreply.github.com>
2025-10-25 11:56:53 +0200

    bugfix/inline-plugin-update (#2) (#3)

--- includes/pages/support_update_tab_update.php
+++ includes/pages/support_update_tab_update.php
@@ -1,5 +1,4 @@
 <?php
-
 if (isset($_POST['umami_connect_self_update']) && check_admin_referer('umami_connect_self_update', 'umami_connect_self_update_nonce')) {
     $update_version = sanitize_text_field($_POST['umami_update_version'] ?? '');
     $release_api_url = 'https://api.github.com/repos/' . UMAMI_CONNECT_GITHUB_USER . '/'. UMAMI_CONNECT_GITHUB_REPO . '/releases/tags/' . urlencode($update_version);
@@ -33,121 +32,18 @@ if (isset($_POST['umami_connect_self_update']) && check_admin_referer('umami_con
         && check_admin_referer('umami_connect_self_update', 'umami_connect_self_update_nonce')
     ) {
         if ($zip_url) {
-        require_once ABSPATH . 'wp-admin/includes/file.php';
-        require_once ABSPATH . 'wp-admin/includes/misc.php';
         require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-        if (!defined('FS_METHOD')) {
-            define('FS_METHOD', 'direct');
-        }
-        global $wp_filesystem;
-        if (empty($wp_filesystem)) {
-            require_once ABSPATH . '/wp-admin/includes/file.php';
-            WP_Filesystem();
-        }
-        $tmp = download_url($zip_url, 60);
-        $plugin_dir = dirname(__DIR__, 2);
-        $backup_zip = $plugin_dir . '-backup.zip';
-        foreach (glob($plugin_dir . '-backup*.zip') as $old_backup) {
-            if (file_exists($old_backup)) {
-                @unlink($old_backup);
+        $upgrader = new Plugin_Upgrader();
+        $result = $upgrader->install($zip_url);
+        if (!is_wp_error($result)) {
+            $main_plugin_file_rel = basename(dirname($plugin_dir)) . '/' . basename($plugin_dir) . '/umami-connect.php';
+            if (!is_plugin_active($main_plugin_file_rel)) {
+                activate_plugin($main_plugin_file_rel);
             }
-        }
-        $unzip_dir = $plugin_dir . '-update';
-        $rollback_error = false;
-        if (!is_wp_error($tmp)) {
-            if (!file_exists($backup_zip)) {
-                if (class_exists('ZipArchive')) {
-                    $zip = new ZipArchive();
-                    if ($zip->open($backup_zip, ZipArchive::CREATE) === TRUE) {
-                        $dir = new RecursiveDirectoryIterator($plugin_dir, RecursiveDirectoryIterator::SKIP_DOTS);
-                        $files = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
-                        foreach ($files as $file) {
-                            $filePath = $file->getRealPath();
-                            $localPath = substr($filePath, strlen($plugin_dir) + 1);
-                            if ($file->isDir()) {
-                                $zip->addEmptyDir($localPath);
-                            } else {
-                                $zip->addFile($filePath, $localPath);
-                            }
-                        }
-                        $zip->close();
-                    }
-                }
-            }
-            if (file_exists($unzip_dir)) {
-                $it = new RecursiveDirectoryIterator($unzip_dir, RecursiveDirectoryIterator::SKIP_DOTS);
-                $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
-                foreach($files as $file) {
-                    if ($file->isDir()) {
-                        @rmdir($file->getRealPath());
-                    } else {
-                        @unlink($file->getRealPath());
-                    }
-                }
-                @rmdir($unzip_dir);
-            }
-            $result = unzip_file($tmp, $unzip_dir);
-            
-            @unlink($tmp);
-            if (!is_wp_error($result)) {
-                $entries = glob($unzip_dir . '/*', GLOB_ONLYDIR);
-                if ($entries && is_dir($entries[0])) {
-                    $new_dir = $entries[0];
-                    $plugin_dir_parent = dirname($plugin_dir);
-                    $plugin_dir_name = basename($plugin_dir);
-                    $archive_dir = $plugin_dir . '-archived-' . date('Ymd-His');
-
-                    if (@rename($plugin_dir, $archive_dir)) {
-                        if (@rename($new_dir, $plugin_dir)) {
-                            @rmdir($unzip_dir);
-                            $main_plugin_file_rel = basename(dirname($plugin_dir)) . '/' . basename($plugin_dir) . '/umami-connect.php';
-                            if (!is_plugin_active($main_plugin_file_rel)) {
-                                activate_plugin($main_plugin_file_rel);
-                            }
-                            $it = new RecursiveDirectoryIterator($archive_dir, RecursiveDirectoryIterator::SKIP_DOTS);
-                            $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
-                            foreach($files as $file) {
-                                if ($file->isDir()) {
-                                    @rmdir($file->getRealPath());
-                                } else {
-                                    @unlink($file->getRealPath());
-                                }
-                            }
-                            @rmdir($archive_dir);
-
-                            echo '<div class="notice notice-success"><b>Update successful!</b> The plugin was updated and reactivated.</div>';
-                            echo '<script>setTimeout(function(){window.location.reload();}, 100);</script>';
-                        } else {
-                            @rename($archive_dir, $plugin_dir);
-                            echo '<div class="notice notice-error"><b>Error:</b> Could not rename new plugin folder. Rollback performed.</div>';
-                        }
-                    } else {
-                        echo '<div class="notice notice-error"><b>Error:</b> Could not archive old plugin folder.</div>';
-                    }
-                } else {
-                    echo '<div class="notice notice-error"><b>Error:</b> Unpacked directory not found.</div>';
-                }
-            } else {
-                $rollback_error = false;
-                if (class_exists('ZipArchive') && file_exists($backup_zip)) {
-                    $zip = new ZipArchive();
-                    if ($zip->open($backup_zip) === TRUE) {
-                        $zip->extractTo(dirname($plugin_dir));
-                        $zip->close();
-                    } else {
-                        $rollback_error = true;
-                    }
-                } else {
-                    $rollback_error = true;
-                }
-                $fs_hint = '';
-                if (is_wp_error($result) && strpos($result->get_error_message(), 'filesystem') !== false) {
-                    $fs_hint = '<br><b>Hint:</b> Check the file system permissions and possibly <code>wp-config.php</code> for <code>define(\'FS_METHOD\', \'direct\');</code>';
-                }
-                echo '<div class="notice notice-error"><b>Error while unpacking:</b> ' . esc_html($result->get_error_message()) . ($rollback_error ? ' Rollback failed!' : ' Backup was restored.') . $fs_hint . '</div>';
-            }
+            echo '<div class="notice notice-success"><b>Update erfolgreich!</b> Das Plugin wurde aktualisiert und reaktiviert.</div>';
+            echo '<script>setTimeout(function(){window.location.reload();}, 100);</script>';
         } else {
-            echo '<div class="notice notice-error"><b>Error during download:</b> ' . esc_html($tmp->get_error_message()) . '</div>';
+            echo '<div class="notice notice-error"><b>Error during update:</b> ' . esc_html($result->get_error_message()) . '</div>';
         }
         } else {
             echo '<div class="notice notice-error"><b>Error:</b> ZIP-URL could not be determined.</div>';
